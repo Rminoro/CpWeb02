aac40cc4738d404d53467a72cc32711bc55c89c8
Rminoro <rminoro@outlook.com>
2023-09-03 19:17:55 -0300

    Conteúdo inicial dos componentes

---
+++ src/routes/EditarProdutos/index.jsx
@@ -0,0 +1,8 @@
+export default fuction EditarProduto(){
+    
+    return(
+        <main>
+        <h1>Editar Produto</h1>
+        </main>
+    )
+}
---
+++ src/routes/Produtos/index.jsx
@@ -0,0 +1,8 @@
+export default function Produtos(){
+
+    return(
+        <>
+            <h1>Error 484 - Página não encontrada</h1>
+        </>
+    )
+}
