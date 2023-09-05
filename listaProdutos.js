8a0bbe234676d4baed8f4e8b265fdfc4aa4357c6
Rminoro <rminoro@outlook.com>
2023-09-05 16:08:52 -0300

    Adicionando 5 itens

--- listaProdutos.js
+++ listaProdutos.js
@@ -2,31 +2,38 @@ import "./src/assets/iphone14promax.jpg"
 export const listaProdutos =[
     {
         id:1,
-        nome:'Samsung Galaxy',
-        preco:2500,
-        descricao:"sad",
+        nome:'Samsung Zflip',
+        preco:"R$4398,30",
+        descricao:"Com a tela dobrável e que tem a exclusiva tecnologia Corning Gorilla Glass Victus, seu Z Flip4 estará pronto para resistir a quedas e arranhões, sua única preocupação será o que você fará primeiro com seu Galaxy Z Flip4 já que as possibilidades são inúmeras!",
+        foto:"./src/assets/Zflip4.png"
     },
     {
         id:2,
         nome:'Iphone 14 Pro max',
         descricao:"iPhone 14 Pro Max. Câmera grande-angular de 48 MP para capturar detalhes impressionantes",
-        preco:10,
-        foto:""
+        preco:"R$ 7599,99",
+        foto:"./src/assets/iphone14promax.jpg"
         
     },
     {
         id:3,
         nome:'Samsung FE S20',
-        preco:30
+        descricao:"Seu celular quase nunca sai de sua mão, por isso ele deve ser um complemento perfeito ao seu estilo. Escolha a partir de uma ampla gama de tons modernos com um acabamento fosco elegante, do chamativo e ousado ao sutil e clássico.",
+        preco:"R$2700",
+        foto:"./src/assets/SamsungFES20.png"
     },
     {
         id:4,
-        nome:'Xiaomi',
-        preco:20
+        nome:'AirPods',
+        descricao:"Com Áudio Espacial Personalizado, que coloca o som ao seu redor,Equalização Adaptativa, que ajusta a música aos seus ouvidos, ",
+        preco:"R$1329,00",
+        foto:"./src/assets/Airpod.png"
     },
     {
         id:5,
-        nome:'Nokia tijolão',
-        preco:25000
-    },
+        nome:'Playstation 5',
+        descricao:"Desfrute do carregamento do seu PS5, extremamente rápido com o SSD de altíssima velocidade, uma imersão mais profunda com suporte a feedback tátil, gatilhos adaptáveis e áudio 3D, além de uma geração inédita de jogos incríveis para PlayStation.",
+        preco:"R$ 3.719,90",
+        foto:"./src/assets/PS5.png"
+    }, 
 ]
--- src/routes/EditarProdutos/index.jsx
+++ src/routes/EditarProdutos/index.jsx
@@ -1,33 +1,52 @@
-import { useParams } from "react-router-dom"
-import { listaProdutos } from "../../../listaProdutos"
-import "./style.css"
+import { useParams } from "react-router-dom";
+import { listaProdutos } from "../../../listaProdutos";
+import "./style.css";
+import Zflip4 from "../../assets/Zflip4.png";
+import iphone14promax from "../../assets/iphone14promax.jpg";
+import SamsungFES20 from "../../assets/SamsungFES20.png";
+import Airpod from "../../assets/Airpod.png";
+import PS5 from "../../assets/PS5.png";
 
+export default function EditarProdutos() {
 
-export default function EditarProdutos(){
-    
     const lista = listaProdutos
     // const navegacao = useNavigate()
-    const {id} = useParams()
+    const { id } = useParams();
 
-    const proc = lista.filter (prod => prod.id == id)
-    const produto = proc[0]
+    const proc = lista.filter(prod => prod.id == id)
+    const produto = proc[0];
 
     // const salvar = () =>{
     //     alert('Produto: ${produto.nome} editado com sucesso!')
     //     return navegacao('/Produtos')
     // }
 
-    return(
+    return (
         <main>
             <h1>{produto.nome}</h1>
             <p>Fotos do {produto.nome} abaixo </p>
             {/* <button onClick={salvar}> Salvar</button> */}
             <p> Descrição: {produto.descricao} </p>
             <p> Preço: {produto.preco} </p>
-            
-            <img id="imgProd"src={produto.foto}></img>
-            
+
+            {produto.nome === "Iphone 14 Pro max" && (
+                <img id="imgProd" src={iphone14promax} alt={produto.nome} />
+            )}
+            {produto.nome === "Samsung Zflip" && (
+                <img id="imgProd" src={Zflip4} alt={produto.nome} />
+            )}
+             {produto.nome === "Samsung FE S20" && (
+                <img id="imgProd" src={SamsungFES20} alt={produto.nome} />
+            )}
+             {produto.nome === "AirPods" && (
+                <img id="imgProd" src={Airpod} alt={produto.nome} />
+            )}
+            {produto.nome === "Playstation 5" && (
+                <img id="imgProd" src={PS5} alt={produto.nome} />
+            )}
         </main>
+            
+        
     
     )
 
